bf61bdaa5c009be99fa0e9580978109e9373718d
123mileena <124420638+123mileena@users.noreply.github.com>
2026-01-07 11:21:12 +0530

    hbl

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Cargo Reports",
+    "version": "17.0.1.0.0",
+    "depends": [
+        "shipping_proxy", "base",
+        "web"
+    ],
+    'data': [
+        'reports/shipment_bl_template.xml',
+        'reports/shipment_bl_action.xml',
+    ],
+
+    "installable": True,
+}
