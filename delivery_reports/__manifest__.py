f610c240bfe2e4caa5104c9b88bd45c299236f43
akshairs <akshairs6031@gmail.com>
2026-01-12 11:09:07 +0530

    delivery_report

---
+++ delivery_reports/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "DELIVERY REPORTS",
+    "version": "17.0.1.0.0",
+    "depends": [
+        "shipping_proxy","base",
+        "web"
+    ],
+    "data": ["reports/delivery_template.xml",
+        "reports/delivery_action.xml",
+    ],
+    "installable": True,
+}
