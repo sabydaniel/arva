1362a3be1e0452dd54bdeef851c3cb79bf692823
akshairs <akshairs6031@gmail.com>
2026-01-16 12:21:24 +0530

    arrival template

---
+++ arrival_notice/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "ARRIVAL NOTICE",
+    "version": "17.0.1.0.0",
+    "depends": [
+        "base",
+        "web",
+        "shipping_proxy",
+    ],
+    "data": [
+        "reports/arrival_template.xml",
+        "reports/arrival_action.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
