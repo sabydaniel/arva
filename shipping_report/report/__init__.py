5f21732922495d941d54a3f7475670de99009604
123mileena <124420638+123mileena@users.noreply.github.com>
2026-02-27 15:56:49 +0530

    Delete shipping_report directory

--- shipping_report/report/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import jobprofit_by_charge_xlsx
-from . import sea_consol_report_xlsx
-from . import ship_report_xlsx
-from . import ship_report_report
-from . import work_volume_report
-from . import unbilled_jobs_xlsx
